6daac19451ca6e7ed89e2e5a915a271f93e59fad
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-03-20 13:56:23 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    // code here
+	    priority_queue<int>pq;
+	    vector<int>result;
+	    for(int i =0; i<n; i++)
+	    pq.push(arr[i]);
+	    
+	    for(int i = 0; i<k; i++)
+	   { 
+	    int top = pq.top();
+	    pq.pop();
+	    result.push_back(top);
+	}
+	return result;
+}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
