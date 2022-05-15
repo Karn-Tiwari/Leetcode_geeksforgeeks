cd64e642f51bf98dbf8c939d7184f325f13a054e
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-05-15 16:33:17 +0530

    Added solution - LeetHub

--- Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.cpp
@@ -1,28 +1,38 @@
 // { Driver Code Starts
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int convertFive(int n);
-
+ 
 // Driver program to test above function
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int n;
-        cin >> n;
-        cout << convertFive(n) << endl;
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    /*you are required to complete this method*/
+    int convertFive(int n)
+    {
+    //Your code here
+    string s = to_string(n);
+    for(int i = 0; i<s.length(); i++)
+    {
+        if(s[i]=='0')
+        {
+            s[i]='5';
+        }
     }
-}// } Driver Code Ends
+    return n = stoi(s);
+    }
+};
 
-
-/*you are required to complete this method*/
-int convertFive(int n) {
-    // Your code here
-string str = to_string(n);// converting number into string
-for(int i = 0; i<str.length(); i++)// traversing the string
-if(str[i]== '0')
-str[i] = '5';
-return stoi(str);// converting string to number
-    
-}
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution obj;
+    	cout<<obj.convertFive(n)<<endl;
+    }
+}  // } Driver Code Ends
