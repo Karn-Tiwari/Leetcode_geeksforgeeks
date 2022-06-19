78482854f10ea594f728bd0fd10f9ae82235d5ae
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-06-19 23:40:25 +0530

    Time: 12 ms (6.92%), Space: 7.4 MB (5.37%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,17 +1,54 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-           string s = "";
+        string ans = "";
+        char c = '0';
         
-        int c = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || c == 1)
-        {
-            c += i >= 0 ? a[i --] - '0' : 0;
-            c += j >= 0 ? b[j --] - '0' : 0;
-            s = char(c % 2 + '0') + s;
-            c /= 2;
+        while(a.length()>b.length()){
+            b = '0'+b;
         }
-        
-        return s;
+        while(a.length()<b.length()){
+            a = '0'+a;
+        }
+        int i = a.length()-1;
+        while(i>-1){
+            if(a[i]=='0'&&b[i]=='0'&&c=='0'){
+                ans = '0'+ans;
+                c = '0';
+            }
+            else if(a[i]=='1'&&b[i]=='0'&&c=='0'){
+                ans = '1'+ans;
+                c = '0';
+            }
+            else if(a[i]=='1'&&b[i]=='1'&&c=='0'){
+                ans = '0'+ans;
+                c = '1';
+            }
+            else if(a[i]=='1'&&b[i]=='0'&&c=='1'){
+                ans = '0'+ans;
+                c = '1';
+            }
+            else if(a[i]=='1'&&b[i]=='1'&&c=='1'){
+                ans = '1'+ans;
+                c = '1';
+            }
+            else if(a[i]=='0'&&b[i]=='1'&&c=='0'){
+                ans = '1'+ans;
+                c = '0';
+            }
+            else if(a[i]=='0'&&b[i]=='0'&&c=='1'){
+                ans = '1'+ans;
+                c = '0';
+            }
+            else if(a[i]=='0'&&b[i]=='1'&&c=='1'){
+                ans = '0'+ans;
+                c = '1';
+            }
+            i--;
+        }
+        if(c=='1'){
+            ans = '1'+ans;
+        }
+        return ans;
     }
 };
