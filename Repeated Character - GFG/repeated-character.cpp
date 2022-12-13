005d9a09a1a507d71bc16667c39a039896168e82
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-12-13 23:21:54 +0530

    Added solution - LeetHub

---
+++ Repeated Character - GFG/repeated-character.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+       for(int i = 0; i<s.size(); i++)
+       {
+           int val = count(s.begin(),s.end(),s[i]);
+           if(val>1)
+           return s[i];
+       }
+       return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
