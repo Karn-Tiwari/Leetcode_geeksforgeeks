ed73d6a71cd6d2e135e030d162626eb7790f3440
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-12-13 22:17:15 +0530

    Added solution - LeetHub

---
+++ Exceptionally odd - GFG/exceptionally-odd.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        unordered_map<int,int>map;
+        for(int i =0; i<n; i++)
+        {
+            map[arr[i]]++;
+        }
+        for(auto val:map)
+        {
+            if(val.second%2!=0)
+            return val.first;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getOddOccurrence(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
