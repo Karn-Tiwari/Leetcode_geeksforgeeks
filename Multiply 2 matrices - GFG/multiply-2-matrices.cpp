1e651f4c276155707a1d999e5b96e7105a45da4b
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2022-12-26 16:52:55 +0530

    Added solution - LeetHub

---
+++ Multiply 2 matrices - GFG/multiply-2-matrices.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    void Mutliply(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+         int n = matrixA.size();
+        vector<vector<int>>ans(n,vector<int>(n,0));
+       
+       for(int i =0; i<n; i++)
+          {
+              for(int j = 0; j<n; j++)
+              {
+                  int sum = 0; 
+                  for(int k =0; k<n; k++)
+                  {
+                      sum = sum + matrixA[i][k]*matrixB[k][j];
+                  }
+                ans[i][j] = sum;
+              }
+          }
+          matrixA = ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrixA(n, vector<int>(n,0));
+		vector<vector<int>> matrixB(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixA[i][j];
+			}
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixB[i][j];
+			}
+		}
+		Solution ob;
+		ob.Mutliply(matrixA, matrixB);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cout << matrixA[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
